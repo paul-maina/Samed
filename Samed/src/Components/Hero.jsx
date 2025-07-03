039948f72e8a23893063d1af7ab272c07414b006
paul-maina <paulkariz66@gmail.com>
2025-07-03 09:57:29 +0000

    s

--- Samed/src/Components/Hero.jsx
+++ Samed/src/Components/Hero.jsx
@@ -2,23 +2,34 @@
 
 
 function Hero(){
+
+    const search=()=>{}
     return(
-        <div>
-            <div className="flex">
-                <div className="flex justify-between items-center gap-5 font-normal pt-5 m-auto">
-                    <a href="/" className="text-blue-800 ">About Us</a>
-                    <a href="/" className="text-blue-800">Services</a>
-                    <a href="/" className="text-blue-800">Blog</a>
-                    <a href="/" className="text-blue-800">FAQ</a>
-                    <a href="/" className="text-blue-800">Contact </a>        
+        <div className="m-5">
+            <div className="flex font-normal p-5">
+                <div className="flex gap-5 justify-center m-auto">
+                    <a href="/" className="text-blue-800 hover:text-teal-300">|About Us|</a>
+                    <a href="/" className="text-blue-800 hover:text-teal-300">|Services|</a>
+                    <a href="/" className="text-blue-800 hover:text-teal-300">|Blog|</a>
+                    <a href="/" className="text-blue-800 hover:text-teal-300">|FAQ|</a>
+                    <a href="/" className="text-blue-800 hover:text-teal-300">|Contact|</a>        
                 </div>
                 <div>
-                    <a href="/" className="text-blue-800 justify-center items-center m-auto pt-5">Login/Sign In </a>
+                    <a href="/" className="text-blue-800 hover:text-teal-300 absolute left-[85%]">Login/Sign In </a>
                 </div>
             </div>
-         <div className="logo">
-            <img src="https://i.imgur.com/J7mTQ3L.png" alt="" className="w-auto h-20 mx-5 px-2"/>
-         </div>
+                <div className="logo flex gap-10">
+                    <img src="https://imgur.com/prS4lTC.png" alt="" className="w-auto h-20 "/>
+                    <form className="w-1/2 text-center m-auto  p-2 rounded-3xl ">
+                        <input 
+                           type="text"
+                           placeholder="search"
+                           className="w-full px-4 py-2 bg-gray-300 text-left placeholder:text-left rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+                           </input>
+
+                    </form>
+
+                </div>
 
 
 
